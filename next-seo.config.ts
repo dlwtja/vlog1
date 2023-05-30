455a934ff557a1467938160d5f76f0a4fe8d7d0e
sung hyun <dlwtja1@naver.com>
2023-05-30 16:26:17 +0900

    seo수정

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -3,11 +3,11 @@ import type { DefaultSeoProps } from "next-seo";
 const config: DefaultSeoProps = {
   title: "Hwang devlog",
   description:
-    "프론트엔드 개발자 Hwang 개발블로그 입니다. 세상에서 가장 빠르게 최신 개발 트렌드를 확인해보세요.",
+    " 개발자 Hwang 개발블로그 입니다. 세상에서 가장 빠르게 최신 개발 트렌드를 확인해보세요.",
   openGraph: {
     title: "Hwang devlog",
     description:
-      "프론트엔드 개발자 Hwang 개발블로그 입니다. 세상에서 가장 빠르게 최신 개발 트렌드를 확인해보세요.",
+      " 개발자 Hwang 개발블로그 입니다. 세상에서 가장 빠르게 최신 개발 트렌드를 확인해보세요.",
     type: "website",
     locale: "ko_KR",
     siteName: "Hwang devlog",
